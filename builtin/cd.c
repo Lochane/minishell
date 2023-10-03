3bb1e7e083a7fc9cc06d29969ba7b09f31ab444d
Lochane <skeleton9111@gmail.com>
2023-10-03 14:42:42 +0200

    modif cd

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: lsouquie <lsouquie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 11:42:40 by lsouquie          #+#    #+#             */
-/*   Updated: 2023/10/03 11:56:00 by lsouquie         ###   ########.fr       */
+/*   Updated: 2023/10/03 14:42:04 by lsouquie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,34 @@ int	do_cd(t_cmd *cmd, t_fd fd, t_data *data)
 {
 	t_lst	*pwd;
 	t_lst	*oldpwd;
-	
-	ft_get_env("PWD", data->env, &pwd);
+	char	*pwdpath;
+
+	(void) fd;
+	pwdpath = ft_get_env("PWD", data->env, &pwd);
 	ft_get_env("OLDPWD", data->env, &oldpwd);
 	if (cmd->arg)
 	{
-		if(cmd->arg[1])
+		if (cmd->arg[1])
 		{
 			printf("too many argument\n"); // TODO sortie d'erreur
-			return(0);
+			return (0);
 		}
-		if(chdir(cmd->arg[0]))
+		if (chdir(cmd->arg[0]))
 		{
 			printf("No such file\n"); // TODO sortie d'erreur
-			return(0);
+			return (0);
 		}
-		if(oldpwd && pwd)
+		if (oldpwd && pwd)
 		{
 			free(oldpwd->data);
-			oldpwd->data = pwd->data;
+			oldpwd->data = ft_join("OLDPWD=", pwdpath, 0);//proteger allloc cas ou ca peut etre null et normal?
+			free(pwd->data);
+			pwdpath = getcwd(NULL, 0);
+			pwd->data = ft_join("PWD=", pwdpath, 0);
+			free(pwdpath);
 		}
 	}
 	else
 		printf("No arguments\n"); // TODO sortie d'erreur
-	(void) fd;
 	return (0);
 }
