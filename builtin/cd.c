56a0ca5cff181761682405b312fd5a6f0af7df36
mathieu <mathieu.daguenet@outlook.com>
2023-10-10 16:39:43 +0200

    cd

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: madaguen <madaguen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 11:42:40 by lsouquie          #+#    #+#             */
-/*   Updated: 2023/10/09 18:16:13 by madaguen         ###   ########.fr       */
+/*   Updated: 2023/10/10 16:38:44 by madaguen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,47 +18,42 @@ int	do_cd(t_cmd *cmd, t_fd *fd, t_data *data)
 {
 	t_lst	*pwd;
 	t_lst	*oldpwd;
-	//char	*oldpwdpath;
+	char	*oldpwdpath;
 	char	*pwdpath;
 
 	(void) fd;
 	(void)cmd;
 	pwdpath = ft_get_env("PWD", 3, data->env, &pwd);
 	ft_get_env("OLDPWD", 6, data->env, &oldpwd);
-	/*if (cmd->arg)
+	if (cmd->arg)
 	{
 		if (cmd->arg[1])
-			return ((2, "too many argument\n", 19), 0);
+			return (write(2, "too many argument\n", 19), 0);
 		if (chdir(cmd->arg[0]))
 			return (write(2, "No such file\n", 14), 0);
 		pwdpath = getcwd(NULL, 0);
 		if (pwd)
 		{
-			oldpwdpath = ft_join("OLDPWD=", pwd->data, 0);
-			if (!oldpwdpath)
-				return (fail_malloc(), errno);//free pwdpath
+			if (oldpwd)
+			{
+				oldpwdpath = ft_join("OLDPWD=", pwd->data, 0);
+				if (!oldpwdpath)
+					return (free(pwdpath), fail_malloc(), errno);//free pwdpath
+				free(oldpwd->data);
+			}
 			free(pwd->data);
+			pwd->data = ft_join("PWD=", pwdpath, 0);
+			if (oldpwd && oldpwdpath)
+				oldpwd->data = oldpwdpath;
 		}
-		pwd->data = ft_join("PWD=", pwdpath, 0);
-		if (oldpwd)
-			free(oldpwd->data);
-
 		if (!pwd)
 		{
-			pwd = ft_new_lst(ft_join("PWD=", pwdpath, 0));//bouvcle infinie si on fait env???
+			pwd = ft_new_lst(ft_join("PWD=", pwdpath, 0));
 			if (!pwd)
-				return (fail_malloc(), errno);
+				return (free(pwdpath), fail_malloc(), errno);
 			ft_add_back(&data->env, pwd);
 		}
-		if (!oldpwd)
-		{
-			oldpwd = ft_new_lst(ft_join("OLDPWD=", pwdpath, 0));
-			if (!oldpwd)
-				return (fail_malloc(), errno);
-			ft_add_back(&data->env, oldpwd);
-		}
 		free(pwdpath);
-		free(oldpwdpath);
 	}
 	else
 		printf("No arguments\n"); // TODO sortie d'erreur*/
